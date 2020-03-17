fac85e8d418bbb28ef2226cbf4bd4d2c7060801f
Injae Lee <ilee.injae@gmail.com>
2020-03-17 19:29:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 5
-second_number = 8
+first_number = 5.7
+second_number = 8.4
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
